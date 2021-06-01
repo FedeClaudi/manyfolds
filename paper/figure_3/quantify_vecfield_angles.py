f0f768c89eb5e397ab335ac7f71db971c33a8731
FedeClaudi <federicoclaudi@protonmail.com>
2021-06-01 16:00:26 +0100

    bump

--- paper/figure_3/quantify_vecfield_angles.py
+++ paper/figure_3/quantify_vecfield_angles.py
@@ -3,6 +3,9 @@ import sys
 sys.path.append("./")
 
 import matplotlib.pyplot as plt
+import numpy as np
+
+from fcutils.plot.figure import clean_axes
 
 from manifold import Plane, embeddings, vectors_fields
 from manifold.rnn import RNN
@@ -10,11 +13,17 @@ from manifold.tangent_vector import get_tangent_vector
 from manifold.maths import angle_between
 
 
+plt.rc("text", usetex=True)
+plt.rc("font", family="serif")
+
 N = 12
 K = 32
-n_points = 20
-F = "identity"
+n_points = 40
+n_sec = 0.25
+
+
 f, ax = plt.subplots()
+clean_axes(f)
 
 
 fields = dict(
@@ -23,30 +32,33 @@ fields = dict(
     second=vectors_fields.second_only,
 )
 
-for n_sec in (0.15, 0.5, 1):
-    for F in fields.keys():
-        M = Plane(
-            embeddings.prepare_plane_to_rn(n=N), n_sample_points=n_points
-        )
-        M.vectors_field = fields[F]
+bins = np.linspace(0, 1, 20)
+for F in fields.keys():
+    M = Plane(embeddings.prepare_plane_to_rn(n=N), n_sample_points=n_points)
+    M.vectors_field = fields[F]
 
-        # create RNN
-        rnn = RNN(M, n_units=3)
-        rnn.traces = []
-        rnn.build_W(k=K, scale=100)
-        rnn.run_points(n_seconds=0.15)
+    # create RNN
+    rnn = RNN(M, n_units=3)
+    rnn.traces = []
+    rnn.build_W(k=K, scale=100)
+    rnn.run_points(n_seconds=n_sec)
 
-        # get angles
-        angles = []
-        for n, trace in enumerate(rnn.traces):
-            rnn_vec = trace.trace[-1] - trace.trace[0]
-            tan_vec = get_tangent_vector(M.points[n], vectors_field=fields[F])
+    # get angles
+    angles = []
+    for n, trace in enumerate(rnn.traces):
+        rnn_vec = trace.trace[-1] - trace.trace[0]
+        if np.linalg.norm(rnn_vec) == 0:
+            continue
+        tan_vec = get_tangent_vector(M.points[n], vectors_field=fields[F])
 
-            angles.append(angle_between(rnn_vec, tan_vec))
+        angles.append(angle_between(rnn_vec, tan_vec))
 
-        ax.hist(angles, label=f"{F}_nsec: {n_sec}")
+    ax.hist(angles, label=f"{F}", bins=bins, alpha=0.5, density=True)
 ax.legend()
 plt.show()
+ax.set(xlabel="$\theta (rad)$", ylabel="density")
+f.savefig(f"paper/figure_3/angles_N_{N}.svg", format="svg")
+
 
 # viz = Visualizer(M, rnn, axes=0, manifold_color='#b8b6d1', point_color='#3838BA', wireframe=False, manifold_alpha=.5)
 # for point in viz.manifold.points:
